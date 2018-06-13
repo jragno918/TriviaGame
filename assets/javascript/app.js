72b29ddc04daa1e540d6ccba6da074149f3e3134
Joe Ragno <joseph.ragno3@gmail.com>
2018-06-13 12:08:33 -0400

    created QA array in js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,94 @@
+var myQuestions = [
+    {
+        questionOne : "What is the name of Thanos home planet?",
+        answers : {
+            "a": "Earth",
+            "b": "Titan",
+            "c": "Knowhere",
+            "d": "Nidavellir",
+        },
+        correctAnswer : "b",
+    },
+    {
+        questionTwo : "What is the name of Thor's hammer?",
+        answers : {
+            "a": "Mjolnir",
+            "b": "StormBreaker",
+            "c": "Gutripper",
+            "d": "Gungnir",
+        },
+        correctAnswer : "a",
+    },
+    {
+        questionThree : "The Hulk is the only Avenger capable of defeating Thanos.",
+        answers : {
+            "a": "True",
+            "b": "False",
+        },
+        correctAnswer : "b",
+    },
+    {
+        questionFour : "Which Avenger did Howard Stark create?",
+        answers : {
+            a: "Iron Man",
+            b: "Captain America",
+            c: "Hulk",
+            d: "Black Widow",
+        },
+        correctAnswer : "b",
+    },
+    {
+        questionFive : "Which Infinity Stone created Vision?",
+        answers : {
+            a: "Space",
+            b: "Time",
+            c: "Mind",
+            d: "Soul",
+        },
+        correctAnswer : "c",
+    },
+    {
+        questionSix : "Black Panther gets his powers from the heart shaped herb.",
+        answers : {
+            "a": "True",
+            "b": "False",
+        },
+        correctAnswer : "a",
+    },
+    {
+        questionSeven : "Only one 'Winter Soldier' was created.",
+        answers : {
+            "a": "True",
+            "b": "False",
+        },
+        correctAnswer : "b",
+    },
+    {
+        questionEight : "Which of the following MCU characters is NOT capable of physically posessing an Infinity Stone?",
+        answers : {
+            a: "Thanos",
+            b: "Adam Warlock",
+            c: "Vision",
+            d: "Black Widow",
+        },
+        correctAnswer : "d",
+    },
+    {
+        questionNine : "Which Infinity Stone was used to create Scarlet Witch?",
+        answers : {
+            a: "Space Stone",
+            b: "Tesseract",
+            c: "The Blue One",
+            d: "All of the above",
+        },
+        correctAnswer : "d",
+    },
+    {
+        questionTen : "Stan Lee has a cameo in every Marvel film.",
+        answers : {
+            "a": "True",
+            "b": "False",
+        },
+        correctAnswer : "a",
+    },
+    ]
